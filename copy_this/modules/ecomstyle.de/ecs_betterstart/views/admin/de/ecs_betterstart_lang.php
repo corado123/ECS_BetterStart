e6302b88d76efcc05d4523c580f14bdaecc0ed74
nickname <info@ecomstyle.de>
2014-10-21 16:46:35 +0200

    Compatibility to 4.9

--- copy_this/modules/ecomstyle.de/ecs_betterstart/views/admin/de/ecs_betterstart_lang.php
+++ copy_this/modules/ecomstyle.de/ecs_betterstart/views/admin/de/ecs_betterstart_lang.php
@@ -19,9 +19,8 @@
 $sLangName = 'Deutsch';
 $aLang = array(
     'charset' => 'ISO-8859-15',
-
-    'SHOP_MODULE_GROUP_ecs_main' => 'Grundeinstellungen',
-    'SHOP_MODULE_ecs_bsstarttpl' => 'Welcome-Text auf der Startseite anzeigen.',
-    'SHOP_MODULE_ecs_bspartnerbox' => 'Partner-Box entfernen.',
-    'SHOP_MODULE_ecs_bsshoplupe' => 'Shoplupe-Box entfernen.',
+    'SHOP_MODULE_GROUP_ecs_main'    => 'Grundeinstellungen',
+    'SHOP_MODULE_ecs_bsstarttpl'    => 'Welcome-Text auf der Startseite anzeigen.',
+    'SHOP_MODULE_ecs_bspartnerbox'  => 'Partner-Box entfernen.',
+    'SHOP_MODULE_ecs_bsshoplupe'    => 'Shoplupe-Box entfernen.',
 );
